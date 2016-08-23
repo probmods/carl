f1e4dfedda276be89510a7c6a46e2d83e1d1b837
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-08-23 16:27:45 -0700

    Change port 3001 to 3005

--- src/perceive/perceive.js
+++ src/perceive/perceive.js
@@ -4,7 +4,7 @@ const express = require('express');
 const sendPostRequest = require('request').post;
 
 const app = express();
-const port = 3001;
+const port = 3005;
 
 
 function failure(response, msg) {
