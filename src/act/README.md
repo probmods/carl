6985a1bd9e1aec6f6660fb3ef24e97ec6814c9cf
Long Ouyang <longouyang@gmail.com>
2016-08-23 14:04:57 -0700

    Stub of act stuff

---
+++ src/act/README.md
@@ -0,0 +1,7 @@
+to use, make a get request like this:
+
+~~~~
+/?uid=<uid>&time=<time>&question=<question>
+~~~~
+
+this will set up a cron job to run at time (a unix time)
---
+++ src/act/act.js
@@ -0,0 +1,24 @@
+// var cron = require('node-cron');
+
+// var task = cron.schedule('*/2 * * * * *', function(){
+//   console.log('running a task every two seconds');
+//   task.stop()
+// });
+
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
