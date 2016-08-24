5207e7b764f20f41844da1dd88bfb404c9691096
Long Ouyang <longouyang@gmail.com>
2016-08-23 22:05:17 -0700

    [learn] work on reading percepts from store

--- src/learn/learn.js
+++ src/learn/learn.js
@@ -2,9 +2,11 @@ const http = require('http');
 const port = 3004;
 const fs = require('fs');
 var webppl = require('webppl');
+const sendPostRequest = require('request').post;
 
 // a terrible hack because fs.readFileSync('learner.wppl','utf8')
 // doesn't work from the main runner that starts all five servers
+// TODO: switch to the __dirname method
 var learnerSource = function() {
   var globalInput = globalStore.input;
   var globalOutput = {theta: gaussian(globalInput.length, 0.1)};
@@ -20,7 +22,7 @@ function requestHandler(request, response) {
   // optionally do something (maybe report on inference progress so far?)
 }
 
-var interval = 10 * 1000; // seconds
+var interval = 3 * 1000; // seconds
 
 var code = ['(',learnerSource.toString(),')','()'].join('');
 var compiled = webppl.compile(code, {verbose: true,
@@ -40,13 +42,25 @@ var updateBeliefs = function(responses) {
 var responses = [];
 
 setInterval(function() {
-  clog('reading from db (TODO)');
-  responses.push(Math.random());
+  clog('reading from db');
+  sendPostRequest('http://127.0.0.1:4000/db/find',
+                  {json: {collection: 'percepts'}},
+                  (error, res, body) => {
+                    if (!error && res.statusCode === 200) {
+                      clog('db read success');
+                    } else {
+                      clog(`db read error:`);
+                      console.log(res.statusCode);
+                      console.log(error)
+                    }
+                  })
 
-  clog('learning');
-  updateBeliefs(responses);
+  // responses.push(Math.random());
 
-  clog('writing to db (TODO)');
+  // clog('learning');
+  // updateBeliefs(responses);
+
+  // clog('writing to db (TODO)');
 }, interval)
 
 function serve() {
