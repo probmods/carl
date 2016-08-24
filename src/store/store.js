3e490550a72425a1428a9a46cfb52979592ef7b0
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-08-23 21:15:12 -0700

    Add user fixtures; refactor store

--- src/store/store.js
+++ src/store/store.js
@@ -3,12 +3,13 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongodb = require('mongodb');
 const sendPostRequest = require('request').post;
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const MongoClient = mongodb.MongoClient;
 const port = 4000;
 const mongoURL = 'mongodb://localhost:27017/sampleme';
-var database = null;
 const handlers = {};
 
 
@@ -25,103 +26,118 @@ function success(response, msg) {
 }
 
 
-function mongoConnectWithRetry(delayInMilliseconds) {
+function mongoConnectWithRetry(delayInMilliseconds, callback) {
   MongoClient.connect(mongoURL, (err, db) => {
     if (err) {
       console.error(`Error connecting to MongoDB: ${err}`);
-      setTimeout(() => mongoConnectWithRetry(delayInMilliseconds), delayInMilliseconds);
+      setTimeout(() => mongoConnectWithRetry(delayInMilliseconds, callback), delayInMilliseconds);
     } else {
-      console.log("[store] connected succesfully to mongodb");
-      database = db;
+      console.log('[store] connected succesfully to mongodb');
+      callback(db);
     }
   });
 }
 
 
-function serve() {
-  
-  mongoConnectWithRetry(2000);
-
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.post('/register-handler', (request, response) => {
-    if (!request.body) {
-      return failure(response, 'register-handler needs post request body');
-    }
-    const collection = request.body.collection;
-    const callbackURL = request.body.callbackURL;
-    if (!collection) {
-      return failure(response, 'register-handler needs collection');
-    }
-    if (!callbackURL) {
-      return failure(response, 'register-handler needs callbackURL');
-    }
-    if (handlers[collection] === undefined) {
-      handlers[collection] = [];
-    }
-    if (_.includes(handlers[collection], callbackURL)) {
-      return success(response, `handler for ${collection} already registered: ${callbackURL}`);
-    }
-    handlers[collection].push(callbackURL);
-    return success(response, `added handler for ${collection}: ${callbackURL}`);
+function addFixtures(db) {
+  const fixturePath = path.join(__dirname, 'fixtures.json');
+  const fixtures = JSON.parse(fs.readFileSync(fixturePath, 'utf8'));
+  _.forEach(fixtures, (entries, collectionName) => {
+    const collection = db.collection(collectionName);
+    console.log(`[store] inserting fixtures into ${collectionName}`);
+    collection.insertMany(entries);
   });
+}
 
-  app.post('/db/find', (request, response) => {
-    if (!request.body) {
-      return failure(response, '/db/find needs post request body');
-    }
-    const collectionName = request.body.collection;
-    if (!collectionName) {
-      return failure(response, '/db/find needs collection');
-    }
-    const query = request.body.query || {};
-    const projection = request.body.projection;
-    const collection = database.collection(collectionName);
-    console.log(`[store] got request to find in ${collectionName} with` +
-                ` query ${JSON.stringify(query)} and projection ${JSON.stringify(projection)}`);
-    collection.find(query, projection).toArray().then((data) => {
-      response.json(data);
-    });
-  });
 
-  app.post('/db/insert', (request, response) => {
-    if (!request.body) {
-      return failure(response, '/db/insert needs post request body');
-    }
-    const collectionName = request.body.collection;
-    if (!collectionName) {
-      return failure(response, '/db/insert needs collection');
-    }
-    console.log(`[store] got request to insert into ${request.body.collection}`);  
-    const collection = database.collection(collectionName);
-    const data = _.omit(request.body, ['collection']);
-    console.log(`[store] inserting data: ${JSON.stringify(data)}`);
-    collection.insert(data, (err, result) => {
-      if (err) {
-        return failure(response, `error inserting data: ${error}`);
-      } else {
-        // Success
-        if (handlers[collectionName]) {
-          // Call handlers that watch this collection
-          handlers[collectionName].forEach((callbackURL) => {
-            console.log(`[store] calling ${collectionName} handler: ${callbackURL}`);
-            sendPostRequest(callbackURL, { json: result }, (error, res, body) => {
-              if (!error && res.statusCode === 200) {
-                console.log(`[store] successfully notified handler ${callbackURL}`);
-              } else {
-                console.error(`[store] error notifying ${callbackURL}: ${error} ${body}`);
-              }
-            });
-          });
-        }
-        return success(response, `successfully inserted data. result: ${JSON.stringify(result)}`);
+function serve() {  
+
+  mongoConnectWithRetry(2000, (database) => {
+
+    addFixtures(database);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.post('/register-handler', (request, response) => {
+      if (!request.body) {
+        return failure(response, 'register-handler needs post request body');
       }
-    });  
-  });
+      const collection = request.body.collection;
+      const callbackURL = request.body.callbackURL;
+      if (!collection) {
+        return failure(response, 'register-handler needs collection');
+      }
+      if (!callbackURL) {
+        return failure(response, 'register-handler needs callbackURL');
+      }
+      if (handlers[collection] === undefined) {
+        handlers[collection] = [];
+      }
+      if (_.includes(handlers[collection], callbackURL)) {
+        return success(response, `handler for ${collection} already registered: ${callbackURL}`);
+      }
+      handlers[collection].push(callbackURL);
+      return success(response, `added handler for ${collection}: ${callbackURL}`);
+    });
 
-  app.listen(port, () => {
-    console.log(`[store] running at http://localhost:${port}`);
+    app.post('/db/find', (request, response) => {
+      if (!request.body) {
+        return failure(response, '/db/find needs post request body');
+      }
+      const collectionName = request.body.collection;
+      if (!collectionName) {
+        return failure(response, '/db/find needs collection');
+      }
+      const query = request.body.query || {};
+      const projection = request.body.projection;
+      const collection = database.collection(collectionName);
+      console.log(`[store] got request to find in ${collectionName} with` +
+                  ` query ${JSON.stringify(query)} and projection ${JSON.stringify(projection)}`);
+      collection.find(query, projection).toArray().then((data) => {
+        response.json(data);
+      });
+    });
+
+    app.post('/db/insert', (request, response) => {
+      if (!request.body) {
+        return failure(response, '/db/insert needs post request body');
+      }
+      const collectionName = request.body.collection;
+      if (!collectionName) {
+        return failure(response, '/db/insert needs collection');
+      }
+      console.log(`[store] got request to insert into ${request.body.collection}`);  
+      const collection = database.collection(collectionName);
+      const data = _.omit(request.body, ['collection']);
+      console.log(`[store] inserting data: ${JSON.stringify(data)}`);
+      collection.insert(data, (err, result) => {
+        if (err) {
+          return failure(response, `error inserting data: ${error}`);
+        } else {
+          // Success
+          if (handlers[collectionName]) {
+            // Call handlers that watch this collection
+            handlers[collectionName].forEach((callbackURL) => {
+              console.log(`[store] calling ${collectionName} handler: ${callbackURL}`);
+              sendPostRequest(callbackURL, { json: result }, (error, res, body) => {
+                if (!error && res.statusCode === 200) {
+                  console.log(`[store] successfully notified handler ${callbackURL}`);
+                } else {
+                  console.error(`[store] error notifying ${callbackURL}: ${error} ${body}`);
+                }
+              });
+            });
+          }
+          return success(response, `successfully inserted data. result: ${JSON.stringify(result)}`);
+        }
+      });
+    });
+
+    app.listen(port, () => {
+      console.log(`[store] running at http://localhost:${port}`);
+    });
+
   });
 
 }
