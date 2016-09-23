050bc64aaec67c851537ec445cda468ad879cee4
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-09-23 10:23:39 -0700

    Test parameter optimization, serialize/deserialize

--- src/tests/test-webppl.js
+++ src/tests/test-webppl.js
@@ -10,16 +10,41 @@ function main() {
     const filepath = path.join(__dirname, filename);
     return webppl.compile(filepath);    
   };
+
+  // basic webppl program without error
   webppl.run(compile('test-success.wppl'), {}, (error: ?string, value: mixed) => {
     assert.strictEqual(error, null);
     assert.notStrictEqual(value, null);
     console.log(`value: ${JSON.stringify(value)}`);
   });
+
+  // basic webppl program with error
   webppl.run(compile('test-error.wppl'), {}, (error: ?string, value: mixed) => {
     assert.notStrictEqual(error, null);
     assert.strictEqual(value, null);
     console.log(`error (expected): ${JSON.stringify(error)}`);
-  });  
+  });
+  
+  // parameter optimization without previous parameters
+  var params = undefined;
+  webppl.run(compile('test-params.wppl'), { initialStore: { params: params } }, (error: ?string, value: mixed) => {
+    assert.strictEqual(error, null);
+    assert.notStrictEqual(value, null);
+    var p_ = value.p_[0].data[0];
+    assert.ok(p_ > 0);
+    params = value;
+  });
+
+  // parameter optimization with previous parameters; check that we optimize some more
+  webppl.run(compile('test-params.wppl'), { initialStore: { params: params } }, (error: ?string, value: mixed) => {
+    assert.strictEqual(error, null);
+    assert.notStrictEqual(value, null);
+    var p_ = value.p_[0].data[0];
+    assert.ok(p_ > params.p_[0].data[0]);
+  });
+
+  console.log('\nAll tests passed.');
+  
 }
 
 main();
