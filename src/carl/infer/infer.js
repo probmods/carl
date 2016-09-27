4aa43ae8c55bb1f1a9da5bdc798237d5d2d86209
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-09-27 10:40:14 -0700

    Register observation handler in infer

--- src/carl/infer/infer.js
+++ src/carl/infer/infer.js
@@ -2,16 +2,44 @@
 
 import http from '../common/http';
 import settings from '../common/settings';
-import { log, error, httpSuccess, httpFailure } from './util'; 
+import { log, error, httpSuccess, httpFailure } from './util';
+
+
+function registerObservationHandler(hostname, port) {
+
+  const storeURL = (`http://${settings.addresses.store.hostname}:` +
+                    `${settings.addresses.store.port}/registerHandler`);
+  
+  const data = {
+    callbackURL: `http://${hostname}:${port}/handleObservation`,
+    collection: 'observations'
+  };
+
+  http.sendPOSTRequest(storeURL, data, (err, result, body) => {
+    if (!err && result && result.statusCode === 200) {
+      log('successfully registered observation handler');
+    } else {
+      error(`failed to register observation handler, will try again`);
+      setTimeout(registerObservationHandler, 2000);
+    }
+  });
+
+}
 
 
 function serve() {
 
-  const port = settings.addresses.observe.port;
-  const hostname = settings.addresses.observe.hostname;
+  const hostname = settings.addresses.infer.hostname;
+  const port = settings.addresses.infer.port;
+
+  function handleObservation(request: RequestWithBody, reponse: Response): ?Response {
+    log(`received observation: ${JSON.stringify(request.body)}`);
+  }
+
+  registerObservationHandler(hostname, port);
   
   http.runServer(
-    { port },
+    { post: { handleObservation }, port },
     () => { log(`running at http://${hostname}:${port}`); });
 
 }
