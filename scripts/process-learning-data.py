14990abc2e1f69e918e3920126ce4ad259afb2ae
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-10-18 14:32:02 -0700

    Analyze new data

--- scripts/process-learning-data.py
+++ scripts/process-learning-data.py
@@ -2,26 +2,41 @@ import json
 import csv
 import os
 
-dir = "./data/lda-stateless-single-step-optimize/1-learner/"
+# Recursively walk directory, converting all json files
 
-runtimes = []
-elbos = []
+dir = "./data/lda-stateless-single-step-optimize-v2/"
 
-filepath = os.path.join(dir, "carl-lda-greedy-instrument-histories")
+def convert(filepath):
+    runtimes = []
+    elbos = []
+    with open(filepath) as data_file:
+        data = json.load(data_file)["histories"]
+        for (key, value) in sorted(data.items()):
+            runtimes.append(int(key))
+            elbos.append(value[0])
+    
+    min_runtime = min(runtimes)
+    runtimes = [runtime-min_runtime for runtime in runtimes]
+    
+    with open("%s.csv" % filepath[:-5], "w") as csvfile:
+        fieldnames = ["RuntimeInSeconds", "ELBO"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    
+        writer.writeheader()
+        for (runtime, elbo) in zip(runtimes, elbos):
+            writer.writerow({"RuntimeInSeconds": runtime/1000, "ELBO": elbo})
 
-with open("%s.json" % filepath) as data_file:
-    data = json.load(data_file)
-    for (key, value) in sorted(data.items()):
-        runtimes.append(int(key))
-        elbos.append(value[0])
-
-min_runtime = min(runtimes)
-runtimes = [runtime-min_runtime for runtime in runtimes]
-
-with open("%s.csv" % filepath, "w") as csvfile:
-    fieldnames = ["RuntimeInSeconds", "ELBO"]
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-    writer.writeheader()
-    for (runtime, elbo) in zip(runtimes, elbos):
-        writer.writerow({"RuntimeInSeconds": runtime/1000, "ELBO": elbo})
+def main():
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file.endswith(".json"):
+                filepath = os.path.join(root, file)                
+                try:
+                    convert(filepath)
+                except ValueError:
+                    print "ERROR",
+                else:
+                    print "OK",
+                print filepath                    
+            
+main()                
--- scripts/viz-learning.r
+++ scripts/viz-learning.r
@@ -1,7 +1,31 @@
-data1 <- read.csv("data/lda-stateless-single-step-optimize/1-learner/carl-lda-greedy-instrument-histories.csv");
-data7 <- read.csv("data/lda-stateless-single-step-optimize/7-learners/carl-lda-flawless-fall-histories.csv");
-pdf("figures/lda-stateless-single-step-optimize/1-vs-7.pdf")
-plot(data7, type="l", col="green", ylab="ELBo")
-lines(data1, col="red")
-legend("bottomright",c("7 learners, lda100, sgd", "1 learner, lda100, sgd"), col=c("green", "red"), lty=1)
+data <- list()
+
+data[[1]] <- read.csv("data/lda-stateless-single-step-optimize-v2/1-learner/carl-lda-square-harbor-histories.csv");
+data[[2]] <- read.csv("data/lda-stateless-single-step-optimize-v2/2-learners/carl-lda-noble-thought-histories.csv");
+data[[3]] <- read.csv("data/lda-stateless-single-step-optimize-v2/4-learners/carl-lda-chummy-chess-histories.csv")
+data[[4]] <- read.csv("data/lda-stateless-single-step-optimize-v2/8-learners/carl-lda-gaudy-representative-histories.csv");
+data[[5]] <- read.csv("data/lda-stateless-single-step-optimize-v2/16-learners/carl-lda-ambiguous-record-histories.csv");
+data[[6]] <- read.csv("data/lda-stateless-single-step-optimize-v2/30-learners/carl-lda-aloof-jail-histories.csv");
+
+legends <- list()
+
+legends[[1]] <- "1 learner";
+legends[[2]] <- "2 learners";
+legends[[3]] <- "4 learners";
+legends[[4]] <- "8 learners";
+legends[[5]] <- "16 learners";
+legends[[6]] <- "30 learners";
+
+pdf("figures/lda-stateless-single-step-optimize-v2/time-vs-elbo.pdf")
+
+plot(data[[1]], xlim=c(0,9000), ylim=c(-48000,-39000), type="n", ylab="ELBo", main="Parallel SGD on LDA100")
+
+colors <- rainbow(6)
+
+for (i in 1:6){
+    lines(data[[i]], col=colors[i])
+}
+
+legend("bottomright", legend=legends, col=colors, lty=1)
+
 dev.off()
