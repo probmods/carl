f21b0560867fb678de2ad9e428e130a2bba4d8e5
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-11-14 17:38:51 -0800

    Add data + figure for second run

--- scripts/v2/viz.r
+++ scripts/v2/viz.r
@@ -1,27 +1,27 @@
 data <- list()
 legends <- list()
 
-basepath <- "../../data/v2/scaling/expt-1";
+basepath <- "../../data/v2/scaling/expt-2";
 
-data[[1]] <- read.csv(file.path(basepath, "1-thread/lda-progress-mongo-6697.csv"));
+data[[1]] <- read.csv(file.path(basepath, "1-thread/lda-progress-mongo-7291.csv"));
 legends[[1]] <- "1 thread";
 
-data[[2]] <- read.csv(file.path(basepath, "2-threads/lda-progress-mongo-2170.csv"));
+data[[2]] <- read.csv(file.path(basepath, "2-threads/lda-progress-mongo-2757.csv"));
 legends[[2]] <- "2 threads";
 
-data[[3]] <- read.csv(file.path(basepath, "4-threads/lda-progress-mongo-2160.csv"));
+data[[3]] <- read.csv(file.path(basepath, "4-threads/lda-progress-mongo-2450.csv"));
 legends[[3]] <- "4 threads";
 
-data[[4]] <- read.csv(file.path(basepath, "8-threads/lda-progress-mongo-2084.csv"));
+data[[4]] <- read.csv(file.path(basepath, "8-threads/lda-progress-mongo-2430.csv"));
 legends[[4]] <- "8 threads";
 
-data[[5]] <- read.csv(file.path(basepath, "16-threads/lda-progress-mongo-2081.csv"));
+data[[5]] <- read.csv(file.path(basepath, "16-threads/lda-progress-mongo-2536.csv"));
 legends[[5]] <- "16 threads";
 
-data[[6]] <- read.csv(file.path(basepath, "30-threads/lda-progress-mongo-2100.csv"));
+data[[6]] <- read.csv(file.path(basepath, "30-threads/lda-progress-mongo-2750.csv"));
 legends[[6]] <- "30 threads";
 
-pdf("../../figures/v2/expt-1-lda-parallel.pdf")
+pdf("../../figures/v2/expt-2-lda-parallel.pdf")
 
 plot(data[[1]]$time, data[[1]]$objective, ylim=c(-48000, -39000), type="n", xlab="Runtime (seconds)", ylab="ELBo", main="LDA");
 
